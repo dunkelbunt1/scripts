5cd629a3aa2d09db7459902b2fd6fba2f3bac77f
Dunkelbunt <35688085+dunkelbunt1@users.noreply.github.com>
2018-08-14 11:53:57 +0200

    Create cleanup-kubernetes.sh

---
+++ cleanup-kubernetes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker rm -f $(docker ps -qa)
+docker volume rm $(docker volume ls -q)
+docker rmi $(docker images -q) -f
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico"
+for dir in $cleanupdirs; do
+  echo "Removing $dir"
+  rm -rf $dir
+done
